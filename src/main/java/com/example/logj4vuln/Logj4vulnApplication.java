db5d31d5d462a91fa5234e0a498c19b514ee6ad4
yoyoadl <yoyo17813@gmail.com>
2021-12-27 17:11:13 +0800

    commit

--- src/main/java/com/example/logj4vuln/Logj4vulnApplication.java
+++ src/main/java/com/example/logj4vuln/Logj4vulnApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Logj4vulnApplication {
 
     public static void main(String[] args) {
+        System.setProperty("tomcat.util.http.parser.HttpParser.requestTargetAllow","|{}");
+        System.setProperty("com.sun.jndi.rmi.object.trustURLCodebase","true");
+        System.setProperty("com.sun.jndi.ldap.object.trustURLCodebase","true");
         SpringApplication.run(Logj4vulnApplication.class, args);
     }
-
 }
--- src/main/java/com/example/rmiservice/RmiExec.java
+++
@@ -1,4 +0,0 @@
-package com.example.rmiservice;
-
-public class RmiExec {
-}
